14aa19ee6e0ea5d877e963d28963f14bd543f663
anuj singh <a.singh@geophy.com>
2019-03-22 20:36:41 +0100

    updated getRdfTypeFromRemote.ttl. Added retry mechanism; reduced the delay time between each iteration of getting rdf:type from remote live DBpedia SPARQL endpoint.

--- getRdfTypeFromRemote.py
+++ getRdfTypeFromRemote.py
@@ -2,12 +2,18 @@ import glob, os
 import pprint
 import time
 from rdflib import Graph
-from SPARQLWrapper import SPARQLWrapper, JSON, POST
+from SPARQLWrapper import SPARQLWrapper, JSON, POST, POSTDIRECTLY, URLENCODED
 from rdflib.plugin import register, Serializer, Parser
 from rdflib import URIRef
 import pickle
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 path = '../../DBpediaChangeSet/01/00'
+notFoundFilePath = '../../changedClasses/notFound.ttl'
+retryFilePath = '../../changedClasses/retry.ttl'
 
 def getAllFilePaths(dirPath):
     allFilePath = []
@@ -22,14 +28,17 @@ def instatitechangedClassHourDict (filePath):
     intialDictionary = {}
     changedClassLog = open(hourlyChangedClass,"r")
     for eachLine in changedClassLog:
-        eachLine = eachLine.decode('utf-8').strip()
+        eachLine = eachLine.strip()
         intialDictionary[eachLine.split('<http://er/c>')[0].strip()] = eachLine.split('<http://er/c>')[1].replace('.','').strip()
     return intialDictionary
 
+
 for filePath in getAllFilePaths(path):
-    print "############################################################"
-    print filePath
-    print "############################################################"
+    print ("############################################################")
+    print (filePath)
+    print ("############################################################")
+    notFoundIRIFile = open(notFoundFilePath,"a")
+    retryFile = open(retryFilePath,"a")
     hourlyChangedClass = filePath.replace('DBpediaChangeSet', 'changedClasses')
     hourlyChangedClass = hourlyChangedClass.replace('/' + hourlyChangedClass.split('/')[len(filePath.split('/')) - 1], '.ttl')
     changedClassHourDict = {}
@@ -42,34 +51,50 @@ for filePath in getAllFilePaths(path):
     file.parse(filePath, format="nt")
     sparqlEndpoint = SPARQLWrapper("http://live.dbpedia.org/sparql")
     for s,p,o in file:
-        time.sleep(.5)
-        print s.encode('utf-8', errors='ignore')
+        time.sleep(.4)
+        print (s)
         query = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         SELECT DISTINCT ?extractedType
         WHERE {
-        OPTIONAL {<""" + s.encode('utf-8', errors='ignore') + """> rdf:type ?type.}
-        OPTIONAL{ <""" + s.encode('utf-8', errors='ignore') + """> <http://dbpedia.org/ontology/wikiPageRedirects> ?redirected.
+        OPTIONAL {<""" + str(s.encode('utf-8')) + """> rdf:type ?type.}
+        OPTIONAL{ <""" + str(s.encode('utf-8')) + """> <http://dbpedia.org/ontology/wikiPageRedirects> ?redirected.
         ?redirected rdf:type ?type1.}
         BIND(COALESCE(?type1, ?type) AS ?rdfType)
-        BIND(COALESCE(?rdfType, "http://www.w3.org/2002/07/owl#Thing") AS ?extractedType)
+        BIND(COALESCE(?rdfType, "http://notFound") AS ?extractedType)
         }"""
-        sparqlEndpoint.setQuery(query)
-        sparqlEndpoint.setMethod(POST)
-        sparqlEndpoint.setReturnFormat(JSON)
-        results = sparqlEndpoint.query().convert()
+        try:
+            query = query.encode('UTF-8')
+            sparqlEndpoint.setQuery(query)
+            sparqlEndpoint.setMethod(POST)
+            sparqlEndpoint.setReturnFormat(JSON)
+            results = sparqlEndpoint.query().convert()
+        except:
+            print "retrying"
+            retryFile.write('<' + str(s.encode('utf-8')) + '>\n')
+            time.sleep(.5)
+            continue
+            #results = sparqlEndpoint.query().convert()
+
         for result in results["results"]["bindings"]:
             key = "<" + result["extractedType"]["value"] + ">"
-            if key in changedClassHourDict:
-                changedClassHourDict[key] = int(changedClassHourDict[key]) + 1
+            if key == '''<http://notFound>''':
+                notFoundIRIFile.write('<' + str(s.encode('utf-8')) + '>\n')
             else:
-                changedClassHourDict[key] = 1
+                if key in changedClassHourDict:
+                    changedClassHourDict[key] = int(str(changedClassHourDict[key]).strip()) + 1
+                else:
+                    changedClassHourDict[key] = 1
 
     filePathMoved = filePath.replace('DBpediaChangeSet', 'DBpediaChangeSetDone')
     changedClassTS = open(hourlyChangedClass,"w+")
     for eachKey in changedClassHourDict.keys():
-        copyTriple = (eachKey + " <http://er/c> "+ str(changedClassHourDict[eachKey])+ " . \n" ).encode('utf-8', errors='ignore')
-        changedClassTS.write(copyTriple)
+        #copyTriple = (eachKey + " <http://er/c> " ).encode('utf-8', errors='ignore')  str(changedClassHourDict[eachKey])+ )
+        changedClassTS.write((eachKey + " <http://er/c> " ).encode('utf-8', errors='ignore'))
+        changedClassTS.write(str(changedClassHourDict[eachKey]))
+        changedClassTS.write(" . \n")
     changedClassTS.close()
+    notFoundIRIFile.close()
+    retryFile.close()
     # Move a file by renaming it's path
     os.rename(filePath, filePathMoved)
     time.sleep(1)
